b2df634bf74addddf5e577de2f2776886bd1c944
Rzt Prog <rztprog@outlook.fr>
2022-06-08 18:45:53 +0200

    Add new create index controller for blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,32 @@
 class BlogsController < ApplicationController
   def index
-    
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+    # authorize @list
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    # @list.user = current_user
+    # authorize @list
+
+    if @list.save
+      redirect_to blog_path(@blog)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  def blog_params
+    params.require(:blog).permit(:title, :post, :image)
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
 
   def index
+    
   end
 
   def contact
+
   end
 end
