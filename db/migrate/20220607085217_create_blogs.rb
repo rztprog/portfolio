9129365d115cc20e24b02b480f81c64851127804
Rzt Prog <rztprog@outlook.fr>
2022-06-08 18:40:35 +0200

    Add fields to blog and validation

--- db/migrate/20220607085217_create_blogs.rb
+++ db/migrate/20220607085217_create_blogs.rb
@@ -1,6 +1,9 @@
 class CreateBlogs < ActiveRecord::Migration[6.1]
   def change
     create_table :blogs do |t|
+      t.string :title
+      t.string :post
+      t.string :image
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,9 @@ ActiveRecord::Schema.define(version: 2022_06_07_085217) do
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "post"
+    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
