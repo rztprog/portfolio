cf55072ef59ab1b36daa967efc9761556cd3766a
Rzt Prog <rztprog@outlook.fr>
2022-06-19 16:39:35 +0200

    Display flash message, add blogback color, change mail header to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,7 +9,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "Contact Form Inquiry",
-      :to => "rztprog@outlook.fr",
+      :to => "RztProg@outlook.fr",
       :from => %("#{name}" <#{email}>)
     }
   end
