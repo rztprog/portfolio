1c9bb24f6eebed9b27262b2dc06ef08e8b0e0f39
Rzt Prog <rztprog@outlook.fr>
2022-06-19 17:34:14 +0200

    Mailcatcher full worked

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,11 @@ Rails.application.configure do
 
   # OWN CONFIG
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  # Default SMTP
+  # config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.smtp_settings = { :address => '192.168.1.62', :port => 1025 }
+  # Add http settings test
+  # config.action_mailer.http_settings = { :address => '192.168.1.62', :port => 1080 }
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
