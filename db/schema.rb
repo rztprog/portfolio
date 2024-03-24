e7ffc3dff560ccc0cdd287aec04f4d2af458c9df
rztprog <rztprog@outlook.fr>
2024-03-24 17:27:51 +0100

    Add fields to blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_24_161453) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_24_162716) do
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "post"
+    t.string "image"
   end
 
 end
