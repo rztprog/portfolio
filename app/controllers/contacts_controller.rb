7f76a5d441eaa226c4f68415f1072a733419831a
rztprog <rztprog@outlook.fr>
2024-04-02 18:03:48 +0200

    rails db:migrate

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,17 +2,17 @@ class ContactsController < ApplicationController
   # add_flash_types :info, :error, :warning
 
   def new
-    @contact = Contact.new
+    # @contact = Contact.new
   end
 
   def create
-    @contact = Contact.new(params[:contact])
-    @contact.request = request
-    if @contact.deliver
+    # @contact = Contact.new(params[:contact])
+    # @contact.request = request
+    # if @contact.deliver
       # flash.now[:success] = 'Message sent!'
-    else
+    # else
       # flash.now[:error] = 'Could not send message !'
-      render :new
-    end
+      # render :new
+    # end
   end
 end
