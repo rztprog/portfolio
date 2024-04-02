aa5770ea63e90b8e0cc375d21b3b04df0d6a41c6
rztprog <rztprog@outlook.fr>
2024-04-02 18:08:59 +0200

    Add contact model

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,18 @@
 class ContactsController < ApplicationController
-  # add_flash_types :info, :error, :warning
+  add_flash_types :info, :error, :warning
 
   def new
-    # @contact = Contact.new
+    @contact = Contact.new
   end
 
   def create
-    # @contact = Contact.new(params[:contact])
-    # @contact.request = request
-    # if @contact.deliver
-      # flash.now[:success] = 'Message sent!'
-    # else
-      # flash.now[:error] = 'Could not send message !'
-      # render :new
-    # end
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:success] = 'Message sent!'
+    else
+      flash.now[:error] = 'Could not send message !'
+      render :new
+    end
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,16 @@
-class Contact < ApplicationRecord
+class Contact < MailForm::Base
+  attribute :name,      validate: true
+  attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message,   validate: true, length: { minimum: 12 }
+  attribute :nickname,  captcha: true # For Bots
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      :subject => "Contact via site",
+      :to => "Rztprog@outlook.fr",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
 end
